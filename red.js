345a2d798c210ab61b9ab234e97652b2804f741b
Miguel Acuña <miguel.acuna@dpp.cl>
2024-01-29 17:57:01 -0300

    Avances

--- red.js
+++ red.js
@@ -1,2 +1,31 @@
 let [lat, long] = args.plainTexts;
-return `${lat} - ${long}`;
+const baseUrl = 'https://appred.tstgo.cl/phone/v8/stopInfo?stopCode=STOPID&mode=3';
+const paraderos = 'https://raw.githubusercontent.com/imiguelacuna/donde-viene-la-micro/main/stops.json';
+
+const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => 6371 * 2 * Math.atan2( Math.sqrt( Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2 ), Math.sqrt( 1 - (Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2) ) ) * 1000;
+const deg2rad = (deg) => deg * (Math.PI / 180);
+
+let req = new Request(paraderos);
+
+const response = await req.loadJSON();
+
+return response;
+/*
+const paradasTemp = stops
+	.map((item) => ({
+		stopId: item.stop_id,
+		name: item.name,
+		distance: Math.round(getDistanceFromLatLonInKm(lat, long, item.latitude, item.longitude)),
+		routes: item.routes
+	}))
+	.filter((item) => item.distance <= distancia);
+
+// Ordenar las paradas por distancia de menor a mayor
+paradasTemp.sort((a, b) => a.distance - b.distance);
+
+const top5 = paradasTemp.slice(0,5);
+
+const consultar = (stopId) => {
+	const url = baseUrl.replace('STOPID', stopId);
+}
+*/

af90e3b5cdfc733ffc797d01a308965c88cd1c79
Miguel Acuña <miguel.acuna.fabres@gmail.com>
2024-01-29 19:15:00 -0300

    Actualizar red.js

--- red.js
+++ red.js
@@ -48,7 +48,7 @@ listadoParaderos.sort((a, b) => a.distance - b.distance);
  
 const menu = listadoParaderos.reduce((acc, curr)=> { 
 	acc[acc.stopId] = acc.name;
-	return menu;
+	return acc;
 }, {});
 
 return { menu, raw: listadoParaderos };

98d492308f2d4222c9fa98835ecc5e9836a898d5
Miguel Acuña <miguel.acuna@dpp.cl>
2024-01-30 11:59:16 -0300

    Avances

--- red.js
+++ red.js
@@ -1,35 +1,51 @@
-let [lat, long] = args.plainTexts;
 const baseUrl = 'https://appred.tstgo.cl/phone/v8/stopInfo?stopCode=STOPID&mode=3';
 const paraderos = 'https://raw.githubusercontent.com/imiguelacuna/donde-viene-la-micro/main/stops.json';
 
-const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => 6371 * 2 * Math.atan2( Math.sqrt( Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2 ), Math.sqrt( 1 - (Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2) ) ) * 1000;
 const deg2rad = (deg) => deg * (Math.PI / 180);
 
-const buscarServicios = async (stopId) => {
-	const url = baseUrl.replace('STOPID', stopId);
-	let req = new Request(url);
-	req.headers = {
-		"phone-id": "123456789",
-	}
-	const response = await req.loadJSON();
+const getDistanceFromLatLonInKm = (lat1, lon1, lat2, lon2) => 6371 * 2 * Math.atan2( Math.sqrt( Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2 ), Math.sqrt( 1 - (Math.sin(deg2rad(lat2 - lat1) / 2) ** 2 + Math.cos(deg2rad(lat1)) * Math.cos(deg2rad(lat2)) * Math.sin(deg2rad(lon2 - lon1) / 2) ** 2) ) ) * 1000;
 
-	return response?.predictions?.map(x => {
-		return {
-			route: x?.route,
-			distanceLabel: x?.distanceLabel,
-			timeLabel: x?.timeLabel,
-			distance: x?.distance,
-			licensePlate: x?.licensePlate,
-		}
-	}) || [];
+const fetchData = async (url, headers = {}) => {
+	let req = new Request(url);
+	req.headers = headers;
+	return await req.loadJSON();
 }
 
-let req = new Request(paraderos);
+const getServices = async (stopId) => {
+	const url = baseUrl.replace('STOPID', stopId);
+	const response = await fetchData(url, {"phone-id": "123456789"});
+	return response?.predictions?.map(x => ({
+		route: x?.route,
+		distanceLabel: x?.distanceLabel,
+		timeLabel: x?.timeLabel,
+		distance: x?.distance,
+		licensePlate: x?.licensePlate,
+	})) || [];
+}
 
-const response = await req.loadJSON();
+const sortStops = (stops) => stops.sort((a, b) => a.distance - b.distance);
 
+const createMenu = (stops) => stops.reduce((acc, curr)=> {
+	acc[curr.stopId] = `${curr.distance} metros - ${curr.name}`;
+	return acc;
+}, {});
 
-const listadoParaderos = response
+const createServices = (stops) => stops.reduce((acc, curr) => {
+	acc[curr.stopId] = curr.servicios.reduce((accServicio, currServicio) => {
+		const info = `[${currServicio.licensePlate}] ${currServicio.timeLabel} (${currServicio.distanceLabel})`;
+		accServicio[currServicio.route] = accServicio[currServicio.route]
+			? accServicio[currServicio.route] + ' - ' + info
+			: info;
+		return accServicio;
+	}, {});
+	return acc;
+}, {});
+
+let [lat, long] = args.plainTexts;
+
+const response = await fetchData(paraderos);
+
+let listadoParaderos = response
 	.map((item) => ({
 		stopId: item.stop_id,
 		name: item.name,
@@ -40,28 +56,12 @@ const listadoParaderos = response
 
 for(let i = 0; i < listadoParaderos.length; i++) {
 	const paradero = listadoParaderos[i];
-	const servicios = await buscarServicios(paradero.stopId);
-	paradero.servicios = servicios;
+	paradero.servicios = await getServices(paradero.stopId);
 }
 
-// Ordenar las paradas por distancia de menor a mayor
-listadoParaderos.sort((a, b) => a.distance - b.distance);
- 
-const menu = listadoParaderos.reduce((acc, curr)=> { 
-	acc[curr.stopId] = `${curr.distance} metros - ${curr.name}`;
-	return acc;
-}, {});
+listadoParaderos = sortStops(listadoParaderos);
 
-const servicios = listadoParaderos.reduce((acc, curr) => {
-	acc[curr.stopId] = curr.servicios.reduce((accServicio, currServicio) => {
-		const info = `[${currServicio.licensePlate}] ${currServicio.timeLabel} (${currServicio.distanceLabel})`;
-		accServicio[currServicio.route] = accServicio[currServicio.route]
-			? accServicio[currServicio.route] + ' - ' + info
-			: info;
-		return accServicio;
-	}, {});
-
-	return acc;
-}, {});
+const menu = createMenu(listadoParaderos);
+const servicios = createServices(listadoParaderos);
 
 return { menu, servicios };
